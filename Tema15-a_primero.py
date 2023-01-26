662dfdb4a61fbdfc9981b9382eb6c5ef754b7b16
Juan <mtzjuanmtz27@gmail.com>
2023-01-25 19:53:54 -0600

    otro commit

---
+++ Tema15-a_primero.py
@@ -0,0 +1,5 @@
+import Tema15_my_module
+
+print('Hola desde primero')
+Tema15_my_module.tema1()
+print('Adios desde primero')
---
+++ Tema15_my_module.py
@@ -0,0 +1,15 @@
+def tema1():
+    print('Hola dese my_module.py')
+
+def tema2():
+    print('Adios dese my_module.py')
+
+def main():
+    print('Hola dese main')
+
+tema1()
+tema2()
+
+
+if __name__ =='__main__':
+    main()
